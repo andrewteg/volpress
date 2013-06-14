d8e53be562c5c230ccdee4ffba04c395bdefd54f
andrewteg <andrewteg@gmail.com>
2013-06-14 17:25:48 -0600

    Update volpress_templates.php

--- volpress_templates.php
+++ volpress_templates.php
@@ -21,6 +21,7 @@ function get_custom_post_type_single_template($single_template) {
 add_filter( 'archive_template', 'get_custom_post_type_archive_template' ) ;
 function get_custom_post_type_archive_template( $archive_template ) {
      global $post;
+     //custom post type archive
 	 if (is_post_type_archive('volpress')) {
 		//get_stylesheet_directory() gets child theme so best for plugin IMHO
 		$theme_file = get_stylesheet_directory().'/archive-volpress.php'; //echo $theme_file;
@@ -32,5 +33,18 @@ function get_custom_post_type_archive_template( $archive_template ) {
 			$archive_template = VOLPRESS_PLUGIN_PATH . 'templates/archive-volpress.php';
 		}
 	 }
+	 //custom taxonomy (categories) archive
+     if (is_tax('volpress-cats')) {
+		//get_stylesheet_directory() gets child theme so best for plugin IMHO
+		$theme_file = get_stylesheet_directory().'/taxonomy-volpress-cats.php'; //echo $theme_file;
+		if (file_exists($theme_file)) {
+			//echo 'theme';
+			$archive_template = $theme_file;
+		} else {
+			//echo 'plugin';
+			$archive_template = VOLPRESS_PLUGIN_PATH . 'templates/taxonomy-volpress-cats.php';
+		}
+     }
+     //give it back updated for the custom stuff
      return $archive_template;
 }
